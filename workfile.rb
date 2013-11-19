fad3b8039713a0b36d869ec48f2a5bcac717d975
Fredrik Eldh <fredrik.eldh@mobilesorcery.com>
2013-11-19 15:56:27 +0100

    Attempted to add plugin com.evothings.ble. Not working so well...

--- workfile.rb
+++ workfile.rb
@@ -9,9 +9,9 @@ if(!File.exist?('platforms/android'))
 	sh 'cordova -d platform add android'
 end
 
-def addPlugin(name)
+def addPlugin(name, location = name)
 	if(!File.exist?("plugins/#{name}"))
-		sh "cordova -d plugin add #{name}"
+		sh "cordova -d plugin add #{location}"
 	end
 end
 addPlugin('org.apache.cordova.battery-status')
@@ -33,5 +33,9 @@ addPlugin('org.apache.cordova.network-information')
 addPlugin('org.apache.cordova.splashscreen')
 addPlugin('org.apache.cordova.vibration')
 addPlugin('org.chromium.socket')
+addPlugin('com.evothings.ble', '../cordova-ble')
+
+# need to change target android version from 17 to 18 now.
+# edit project.properties and AndroidManifest.xml.
 
 sh 'cordova build android'

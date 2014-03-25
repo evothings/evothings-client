687a3f060e184dba9ff9c94f32f9eb04bfc3ce43
Mikael Kindborg <mikael.kindborg@gmail.com>
2014-03-25 13:34:21 +0100

    Updated Android release build script (keystore file has been moved).

--- build-android-release.sh
+++ build-android-release.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #
 # Script that builds Android release apk
-echo "Building Android release APK"
 #
-# Good to have:
-# chmod u+x build-android-release.sh
-# keytool -genkey -v -keystore evothings.keystore -alias evothings_alias -keyalg RSA -keysize 2048 -validity 365000
+# Make shell script executable:
+#   chmod u+x build-android-release.sh
+# Create a signing key
+#   keytool -genkey -v -keystore evothings.keystore -alias evothings_alias -keyalg RSA -keysize 2048 -validity 365000
+#
+echo "Building Android release APK"
 cordova build android --release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore evothings.keystore platforms/android/bin/EvoThings-release-unsigned.apk evothings_alias
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../keys/evothings.keystore platforms/android/bin/EvoThings-release-unsigned.apk evothings_alias
 jarsigner -verify -verbose platforms/android/bin/EvoThings-release-unsigned.apk
 zipalign -v 4 platforms/android/bin/EvoThings-release-unsigned.apk platforms/android/bin/EvoThings.apk

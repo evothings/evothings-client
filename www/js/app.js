36addb803cc45f039e54b6592f9b822766e654b3
Mikael Kindborg <mikael.kindborg@gmail.com>
2014-05-06 15:23:15 +0200

    Implemented continuous scan.

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,13 @@
-// Application code for the EvoThings client.
+// Application code for the Evothings client.
 
 var app = {}
-app.serverList = []
+app.serverList = {}
 app.socketId = -1
+app.broadcastTimer = null
 
 app.initialize = function()
 {
-    app.bindEvents()
-}
-
-// Bind Event Listeners
-//
-// Bind any events that are required on startup. Common events are:
-// 'load', 'deviceready', 'offline', and 'online'.
-app.bindEvents = function()
-{
-    document.addEventListener('deviceready', app.onDeviceReady, false)
+	document.addEventListener('deviceready', app.onDeviceReady, false)
 }
 
 app.onDeviceReady = function()
@@ -24,17 +16,15 @@ app.onDeviceReady = function()
 	// The Scan button is blank initially, and the text
 	// of the button is set here, when device is ready.
 	// TODO: Would be better to diable the button and
-	// enable it when device is ready.
+	// enable it when device is ready?
 	app.setScanButtonStateToReadyToScan()
 }
 
 // Important to always close socket when page reloads/closes!
+// Note: beforeunload does not work on iOS!
 window.addEventListener('beforeunload', function(e)
 {
-	if (app.socketId > -1)
-	{
-		chrome.socket.destroy(app.socketId)
-	}
+	app.destroySocketAndTimer()
 })
 
 // Set the url field to the saved value.
@@ -49,20 +39,20 @@ app.setSavedIpAddress = function()
 app.connect = function()
 {
 	// Get contents of url text field.
-    var ip = document.getElementById("hyper-url").value
+	var ip = document.getElementById("hyper-url").value
 
-    // Add protocol and port if needed.
-    var url = app.parseIpAddress(ip)
+	// Add protocol and port if needed.
+	var url = app.parseIpAddress(ip)
 
-    // Add url to list and connect.
-    if (url)
-    {
-    	app.connectTo(url)
-    }
-    else
-    {
-    	alert('Malformed URL: ' + url)
-    }
+	// Add url to list and connect.
+	if (url)
+	{
+		app.connectTo(url)
+	}
+	else
+	{
+		alert('Malformed URL: ' + url)
+	}
 }
 
 app.scan = function()
@@ -103,17 +93,22 @@ app.scan = function()
 			0,
 			function(result)
 			{
-				// TODO: Error handling?
-				// result 0 == success
+				// result 0 means success
 				if (result == 0)
 				{
+					// Broadcast UDP packet.
 					send()
+
+					// Timer to do continuous broadcast.
+					app.broadcastTimer = setInterval(function() { send() }, 2000)
+
+					// Start receiving data.
+					recv()
 				}
 				else
 				{
-					app.setScanButtonStateToNormal()
-					// TODO: When bind fails, try to destroy all sockets
-					// and scan again. However limit the number of attempts.
+					app.setScanButtonStateToError()
+					// TODO: When bind fails, try to destroy all sockets.
 				}
 			}
 		)
@@ -128,16 +123,12 @@ app.scan = function()
 			4088,
 			function(writeInfo)
 			{
-				// TODO: Error handling?
-				// writeInfo.bytesWritten == 16 bytes
-				// ('hyper.whoIsThere') means success.
-				if (writeInfo.bytesWritten > 0)
+				// writeInfo.bytesWritten 16 means success
+				// (length of 'hyper.whoIsThere').
+				if (writeInfo.bytesWritten != 16)
 				{
-					recv()
-				}
-				else
-				{
-					app.setScanButtonStateToNormal()
+					// TODO: Error handling?
+					app.setScanButtonStateToError()
 				}
 			}
 		)
@@ -158,39 +149,30 @@ app.scan = function()
 						var data = JSON.parse(bufferToString(recvInfo.data))
 						data.url = 'http://' + ip + ':' + data.port
 						data.ipAndPort = ip + ':' + data.port
-						app.serverList.push(data)
+						app.serverList[ip] = data
 						app.displayServers()
-						// Call recv again to get data from other servers if any.
+
+						// Call recv again to get more data.
 						recv()
 					}
 					catch (err)
 					{
 						// TODO: Error handling?
-						console.log('EvoThingsClient recv error: ' + err)
+						console.log('EvothingsClient recv error: ' + err)
 					}
 				}
-				// TODO: Keep scanning state on button in case multiple
-				// servers will respond?
-				// This could be implemented with a timer, but just
-				// reset the button to normal for now.
-				app.setScanButtonStateToNormal()
 			}
 		)
 	}
 
 	// Clear server list.
-	app.serverList = []
+	app.serverList = {}
+
+	// Clean up existing scan if any.
+	app.destroySocketAndTimer()
 
 	// Set button state.
 	app.setScanButtonStateToScanning()
-
-	// Unbind socket in case scan is called multiple times.
-	// Important to do this to free up the port before
-	// binding the new socket.
-	if (app.socketId > -1)
-	{
-		chrome.socket.destroy(app.socketId)
-	}
 
 	// Open UDP connection.
 	chrome.socket.create('udp', {}, function(createInfo)
@@ -200,14 +182,28 @@ app.scan = function()
 	})
 }
 
+app.destroySocketAndTimer = function()
+{
+	if (app.broadcastTimer)
+	{
+		clearInterval(app.broadcastTimer)
+		app.broadcastTimer = null
+	}
+
+	if (app.socketId > -1)
+	{
+		chrome.socket.destroy(app.socketId)
+		app.socketId = -1
+	}
+}
+
 app.connectTo = function(url)
 {
 	// Save the connect URL.
-    localStorage.setItem('hyper-saved-url', url)
+	localStorage.setItem('hyper-saved-url', url)
 
-	// Destroy the socket so that the UDP port is free.
-	chrome.socket.destroy(app.socketId)
-	app.socketId = -1
+	// Clean up.
+	app.destroySocketAndTimer()
 
 	// If the new page has not loaded within this timeout,
 	// we consider it an error and display a message.
@@ -220,7 +216,7 @@ app.connectTo = function(url)
 
 	// Set button to 'Connecting' state and open the url.
 	app.setConnectButtonStateToConnecting()
-    window.location.assign(url)
+	window.location.assign(url)
 }
 
 // Add protocol and port if needed.
@@ -228,40 +224,40 @@ app.parseIpAddress = function(ip)
 {
 	var url = ip.trim()
 
-    if (!url.match('^https?://[A-Z0-9\.]*.*$'))
-    {
-    	// Add protocol.
-    	url = 'http://' + url
-    }
+	if (!url.match('^https?://[A-Z0-9\.]*.*$'))
+	{
+		// Add protocol.
+		url = 'http://' + url
+	}
 
-    if (url.match('^https?://[0-9\.]*$') &&
-    	!url.match('^https?://[0-9\.]*:[0-9]*$') )
-    {
-    	// Add port to numeric ip address.
-    	url = url + ':4042'
-    }
+	if (url.match('^https?://[0-9\.]*$') &&
+		!url.match('^https?://[0-9\.]*:[0-9]*$') )
+	{
+		// Add port to numeric ip address.
+		url = url + ':4042'
+	}
 
 	return url
 }
 
 app.displayServers = function()
 {
-    var servers = app.serverList
+	var servers = app.serverList
 
-    var list = '' //'<li data-role="list-divider" data-theme="a">EvoThings Studio Servers</li>'
+	var list = '' //'<li data-role="list-divider" data-theme="a">EvoThings Studio Servers</li>'
 
 	if (servers.length <= 0)
 	{
 		list +=
 			'<li>'
 			+	'<h2>No servers found</h2>'
-        	+ '</li>'
+			+ '</li>'
 	}
 	else
 	{
-		for (var i = 0; i < servers.length; ++i)
+		for (var key in servers)
 		{
-			var server = servers[i]
+			var server = servers[key]
 			list +=
 				'<li><a onclick="app.connectTo(\'' +
 				server.url + '\')">' +
@@ -270,25 +266,20 @@ app.displayServers = function()
 				'<small>' + server.ipAndPort + '</small><br/>' +
 				'</a></li>'
 		}
-    }
+	}
 
-    $('#hyper-server-list').html(list)
-    $('#hyper-server-list').listview('refresh')
+	$('#hyper-server-list').html(list)
+	$('#hyper-server-list').listview('refresh')
 }
 
 app.setConnectButtonStateToConnecting = function()
 {
-	$('#hyper-button-connect .ui-btn-text').html('Connecting...')
+	$('#hyper-button-connect .ui-btn-text').html('Connecting')
 }
 
 app.setConnectButtonStateToNormal = function()
 {
 	$('#hyper-button-connect .ui-btn-text').html('Connect')
-}
-
-app.setScanButtonStateToScanning = function()
-{
-	$('#hyper-button-scan .ui-btn-text').html('Scanning...')
 }
 
 app.setScanButtonStateToReadyToScan = function()
@@ -303,13 +294,23 @@ app.setScanButtonStateToReadyToScan = function()
 	}
 	else
 	{
-		$('#hyper-button-scan .ui-btn-text').html('Scan to connect')
+		app.setScanButtonStateToNormal()
 	}
 }
 
 app.setScanButtonStateToNormal = function()
 {
-	$('#hyper-button-scan .ui-btn-text').html('Scan again')
+	$('#hyper-button-scan .ui-btn-text').html('Scan to connect')
+}
+
+app.setScanButtonStateToScanning = function()
+{
+	$('#hyper-button-scan .ui-btn-text').html('Scanning')
+}
+
+app.setScanButtonStateToError = function()
+{
+	$('#hyper-button-scan .ui-btn-text').html('Scan failed')
 }
 
 app.openBrowser = function(url)

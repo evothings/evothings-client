bcee118f39664b0142b881199af6cd81e30ad806
Mikael Kindborg <mikael.kindborg@gmail.com>
2014-05-15 10:37:37 +0200

    Updated Android release build script for Cordova 3.4.1 (path of apk file has changed).

--- build-android-release.sh
+++ build-android-release.sh
@@ -9,6 +9,6 @@
 #
 echo "Building Android release APK"
 cordova build android --release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../keys/evothings.keystore platforms/android/bin/EvoThings-release-unsigned.apk evothings_alias
-jarsigner -verify -verbose platforms/android/bin/EvoThings-release-unsigned.apk
-zipalign -v 4 platforms/android/bin/EvoThings-release-unsigned.apk platforms/android/bin/EvoThings.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../keys/evothings.keystore platforms/android/ant-build/Evothings-release-unsigned.apk evothings_alias
+jarsigner -verify -verbose platforms/android/ant-build/Evothings-release-unsigned.apk
+zipalign -v 4 platforms/android/ant-build/Evothings-release-unsigned.apk platforms/android/ant-build/EvothingsClient.apk

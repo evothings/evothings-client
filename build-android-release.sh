262e211e30f7c11008f55bc29532966f268a0001
Fredrik Eldh <fredrik.eldh@evothings.com>
2015-09-14 11:55:57 +0200

    build-android-release: updated apk paths.

--- build-android-release.sh
+++ build-android-release.sh
@@ -9,6 +9,6 @@
 #
 echo "Building Android release APK"
 cordova build android --release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../keys/evothings.keystore platforms/android/ant-build/Evothings-release-unsigned.apk evothings_alias
-jarsigner -verify -verbose platforms/android/ant-build/Evothings-release-unsigned.apk
-zipalign -v 4 platforms/android/ant-build/Evothings-release-unsigned.apk platforms/android/ant-build/EvothingsClient.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../keys/evothings.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk evothings_alias
+jarsigner -verify -verbose platforms/android/build/outputs/apk/android-release-unsigned.apk
+zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/EvothingsClient.apk
